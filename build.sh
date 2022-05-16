8c2d3203ff68969dd73d821bea2ed67444ba6ac1
Jonathan BEN-AVRAHAM <jonathan.ba@siklu.com>
2022-05-16 08:27:47 +0300

    Replaced linux .config with siklu_277_v2_defconfig
    Replaced linux .config with siklu_277_v2_defconfig and changed build.sh
    appropriately. Also added -eu and other tweaks tioio build.sh.

--- build.sh
+++ build.sh
@@ -1,11 +1,14 @@
-#!/bin/bash
+#!/bin/bash -eu
 
 PATH=/opt/arm/gcc-linaro-7.5.0-2019.12-i686_arm-linux-gnueabihf/bin:$PATH
 export INSTALL_MOD_PATH=$(pwd)/modules
 export ARCH=arm
 export CROSS_COMPILE=arm-linux-gnueabihf-
 
-make menuconfig
+#make savedefconfig
+#make menuconfig
+
+make siklu_PCB277_V2_defconfig
 make clean
 make zImage
 make modules
