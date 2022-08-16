9d93e199903d3ce5da0e4ceff6d9ae51047aa4f9
yba-siklu <84063810+yba-siklu@users.noreply.github.com>
2022-08-16 19:37:24 +0300

    PORTF-1130 Changed build.sh GCC to 10.2.2020.11 (#3)

--- build.sh
+++ build.sh
@@ -1,14 +1,13 @@
 #!/bin/bash -eu
 
-PATH=/opt/arm/gcc-linaro-7.5.0-2019.12-i686_arm-linux-gnueabihf/bin:$PATH
+PATH=/opt/arm/gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf/bin:$PATH
 export INSTALL_MOD_PATH=$(pwd)/modules
 export ARCH=arm
-export CROSS_COMPILE=arm-linux-gnueabihf-
-
-#make savedefconfig
-#make menuconfig
+export CROSS_COMPILE=arm-none-linux-gnueabihf-
 
 make siklu_PCB277_V2_defconfig
+#make menuconfig
+#make savedefconfig
 make clean
 make zImage
 make modules
